f7e0d7e13ccf186bebb1f779f7e282e5e8a0cb99
Harvar3105 <Petrochenko03@inbox.ru>
2023-10-01 22:53:26 +0300

    Git Started

---
+++ Scripts/blue_slime.gd
@@ -0,0 +1,50 @@
+extends CharacterBody2D
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var idle_anim = get_node("CollisionShape2D/Animation")
+
+const SPEED = 100
+var health = 10
+
+var targetEntity = null
+
+var wanderState = true
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	
+	idle_anim.play("Idle")
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if health <= 0:
+		queue_free()
+	
+	if !wanderState:
+		var direction = targetEntity.position.x - position.x
+		print(direction)
+		if (direction > 0):
+			velocity.x = SPEED * 1
+			get_node("CollisionShape2D/Sprite2D").flip_h = true
+		else:
+			velocity.x = SPEED * -1
+			get_node("CollisionShape2D/Sprite2D").flip_h = false
+	
+	move_and_slide()
+
+
+func _on_alert_left_body_entered(body):
+	if body.name == "Player":
+		targetEntity = body
+		wanderState = false
+
+
+func _on_alert_right_body_entered(body):
+	if body.name == "Player":
+		targetEntity = body
+		wanderState = false
+
+
---
+++ Scripts/main_ch.gd
@@ -0,0 +1,33 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@onready var idleAnim = get_node("CollisionShape2D/Idle")
+
+func _physics_process(delta):
+	idleAnim.play("Idle")
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if Input.is_action_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = 0
+	
+	if direction == -1:
+		get_node("CollisionShape2D/Sprite2D").flip_h = true
+
+	elif direction == 1:
+		get_node("CollisionShape2D/Sprite2D").flip_h = false
+	
+	move_and_slide()
+
